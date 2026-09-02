8d98be0e753900da359ba5fada5ee527f8ce314a
andstyle <andermuchael@gmail.com>
2026-09-02 15:14:44 -0300

    Garante carregamento do CSS dos cartões após o estilo principal

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AuthGate from "./AuthGate.jsx";
+import "../public/grid-cartoes-etapas.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

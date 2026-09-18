53f897b233816e37e582dd771f9dbf5de9276bfd
andstyle <andermuchael@gmail.com>
2026-09-18 11:12:17 -0300

    Adiciona Firebase Storage ao chat interno

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Configuração do aplicativo Web Firebase.
 // No Netlify, os valores são fornecidos pelas variáveis VITE_FIREBASE_*.
@@ -30,3 +31,4 @@ export const app = firebaseConfigurado
 
 export const db = firebaseConfigurado ? getFirestore(app) : null;
 export const auth = firebaseConfigurado ? getAuth(app) : null;
+export const storage = firebaseConfigurado ? getStorage(app) : null;
